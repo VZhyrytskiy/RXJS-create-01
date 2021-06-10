6818bfce59ea5cbf8f09fc2480bce12805f93ba3
upskill js.basics <upskill.js.basics@gmail.com>
2021-06-10 11:11:39 +0300

    fix: replace deprecated signature with new one

--- src/01-creation/13-throw-error.ts
+++ src/01-creation/13-throw-error.ts
@@ -1,13 +1,12 @@
 // throwError(
-//   error: any, 
-//   scheduler?: ShedulerLike
+//   errorFactory: any, 
 // ): Observable<never>
 
 import { throwError } from 'rxjs';
 import { addItem, run } from './../03-utils';
 
 export function throwErrorDemo() {
-  const stream$ = throwError(new Error(`This is an error!`));
+  const stream$ = throwError(() => new Error(`This is an error!`));
 
   // run(stream$);
 }
